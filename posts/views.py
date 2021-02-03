4c3ba6928c45ddf4514bf0f855c52add6356a4d5
Sourabh Burse <sourabh.burse@gmail.com>
2021-02-04 00:44:25 +0530

    Search working

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from django.db.models import Count, Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 from marketing.models import Signup
+
+
+def search(request):
+    queryset = Post.objects.all()
+    query = request.GET.get('q')
+    if query:
+        queryset = queryset.filter(
+            Q(title__icontains=query) |
+            Q(overview__icontains=query)
+        ).distinct()
+
+    context = {
+        'queryset': queryset
+    }
+
+    return render(request, 'search_result.html', context)
+
+
+def get_category_count():
+    queryset = Post.objects \
+        .values('categories__title') \
+        .annotate(Count('categories__title'))
+    return queryset
 
 
 def index(request):
@@ -21,8 +46,28 @@ def index(request):
 
 
 def blog(request):
-    return render(request, 'blog.html', {})
+    category_count = get_category_count()
+    post_list = Post.objects.all()
+    most_recent = Post.objects.order_by('-timestamp')[:3]
+    paginator = Paginator(post_list, 4)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except  EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+
+    context = {
+        'queryset': paginated_queryset,
+        'most_recent': most_recent,
+        'page_request_var': page_request_var,
+        'cat_count': category_count
+
+    }
+    return render(request, 'blog.html', context)
 
 
-def post(request):
+def post(request, id):
     return render(request, 'post.html', {})
